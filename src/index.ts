65a58e12a5e987cd04394dd12ca22b375f09a749
baxelson12 <bradaxelson@gmail.com>
2025-05-01 18:16:22 -0400

    chore: init

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Greets a user with given 'name'.
+ *
+ * @param name The user's name
+ * @returns void
+ * @example
+ * ```typescript
+ * import { greet } from 'hyperliquidx';
+ *
+ * // Will log 'Hello, Jimmy!'
+ * greet("Jimmy");
+ * @remarks This is just a test function to ensure everything works properly.
+ */
+export function greet(name: string): string {
+	return `Hello, ${name}!`;
+}
